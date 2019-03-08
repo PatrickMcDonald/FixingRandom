a97396b86469438f9db88bca2e83f0510f23c8d2
Patrick McDonald <PaddyMcDonald@gmail.com>
2019-03-08 13:45:21 +0000

    Fixing Random, part 2

---
+++ FixingRandom/Pseudorandom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace FixingRandom
+{
+    public static class Pseudorandom
+    {
+        private readonly static ThreadLocal<Random> prng = new ThreadLocal<Random>(() => new Random(BetterRandom.NextInt()));
+
+        public static int NextInt() => prng.Value.Next();
+
+        public static double NextDouble() => prng.Value.NextDouble();
+    }
+}
